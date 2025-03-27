85583762807fb2586d60ae41f052f7006f57e282
Stanislau Schastny <stasschastny@gmail.com>
2025-03-27 20:26:03 +0100

    add Lesson 28

--- src/main/java/pages/AccountListPage.java
+++ src/main/java/pages/AccountListPage.java
@@ -32,7 +32,7 @@ public class AccountListPage extends BasePage {
     }
 
     public AccountPage clickOnAccountName(String accountName) {
-        $x(String.format(ACCOUNT_NAME_FIELD_XPATH, accountName)).shouldBe(Condition.visible, Duration.ofSeconds(15)).click();
+        $x(String.format(ACCOUNT_NAME_FIELD_XPATH, accountName)).shouldBe(Condition.visible).click();
         return new AccountPage();
     }
 }
